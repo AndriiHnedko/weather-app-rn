1d837131c9237a35f887d438a1b2eb4468b76a0e
Andrii Hnedko <andrii.hnedko@gmail.com>
2021-02-09 17:37:53 +0200

    added custom map style

--- source/Screens/Map/index.tsx
+++ source/Screens/Map/index.tsx
@@ -1,17 +1,35 @@
 import React, { memo } from 'react';
-import { StyleSheet, View } from 'react-native';
-import MapView, { MAP_TYPES } from 'react-native-maps';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { customMapStyle } from '../../Services/customMapStyle';
 
 const Map = memo(() => {
   return (
-    <View style={{ flex: 1 }}>
-      <MapView
-        provider={'google'}
-        mapType={MAP_TYPES.TERRAIN}
-        style={{ ...StyleSheet.absoluteFillObject }}
-      />
-    </View>
+    <SafeAreaView style={[styles.area]}>
+      <StatusBar barStyle="default" backgroundColor={'#000'} />
+      <View style={[styles.container]}>
+        <MapView
+          provider={'google'}
+          customMapStyle={customMapStyle}
+          // mapType={MAP_TYPES.TERRAIN}
+          style={styles.map}
+        />
+      </View>
+    </SafeAreaView>
   );
+});
+
+const styles = StyleSheet.create({
+  area: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
 });
 
 export default Map;
