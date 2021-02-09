8f112a735fd22af09272b5b5b7fc11bf01e180eb
Andrii Hnedko <andrii.hnedko@gmail.com>
2021-02-09 17:05:33 +0200

    added react-native-maps to project

--- source/Screens/Map/index.tsx
+++ source/Screens/Map/index.tsx
@@ -1,7 +1,17 @@
 import React, { memo } from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView, { MAP_TYPES } from 'react-native-maps';
 
 const Map = memo(() => {
-  return <></>;
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView
+        provider={'google'}
+        mapType={MAP_TYPES.TERRAIN}
+        style={{ ...StyleSheet.absoluteFillObject }}
+      />
+    </View>
+  );
 });
 
 export default Map;
