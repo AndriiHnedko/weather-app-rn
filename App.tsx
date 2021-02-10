c4fe9936304e99b48880b7223a05f88504d3be47
Andrii Hnedko <andrii.hnedko@gmail.com>
2021-02-10 12:56:32 +0200

    installed last version rn reanimated

--- App.tsx
+++ App.tsx
@@ -3,13 +3,17 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './source/Navigation';
+import { Provider } from 'react-redux';
+import Store from './source/Redux/index';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Navigation />
-      </NavigationContainer>
+      <Provider store={Store}>
+        <NavigationContainer>
+          <Navigation />
+        </NavigationContainer>
+      </Provider>
     </SafeAreaProvider>
   );
 };
