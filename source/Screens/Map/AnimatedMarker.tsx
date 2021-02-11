ea48b8eb8736871c7eeb6e8ba6cc63eac6f1c76a
Andrii Hnedko <andrii.hnedko@gmail.com>
2021-02-11 20:00:44 +0200

    create animated marker

---
+++ source/Screens/Map/AnimatedMarker.tsx
@@ -0,0 +1,51 @@
+import React, { memo, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Fontisto';
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+
+type PropsType = {
+  opacity: number;
+  translateY: number;
+  duration: number;
+};
+
+const AnimatedMarker = memo<PropsType>((props) => {
+  const opacity = useSharedValue(0);
+  const translateY = useSharedValue(-20);
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: translateY.value }],
+    opacity: opacity.value,
+  }));
+
+  const initAnimation = () => {
+    translateY.value = withTiming(props.translateY, {
+      duration: props.duration,
+      easing: Easing.out(Easing.exp),
+    });
+    opacity.value = withTiming(props.opacity, {
+      duration: props.duration,
+      easing: Easing.out(Easing.exp),
+    });
+  };
+
+  useEffect(initAnimation);
+
+  return (
+    <Animated.View style={[styles.marker, animatedStyle]}>
+      <Icon name={'map-marker-alt'} size={30} color={'#fb8926'} />
+    </Animated.View>
+  );
+});
+
+const styles = StyleSheet.create({
+  marker: {
+    marginTop: 6,
+  },
+});
+
+export default AnimatedMarker;
